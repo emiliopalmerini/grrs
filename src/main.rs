2cd6047497e512bb07dc6771f062c0f7d9589920
emiliopalmerini <emilio.palmerini@gmail.com>
2022-11-01 22:08:06 +0100

    Read from file

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ struct Cli {
 }
 fn main() {
     let args = Cli::parse();
-    println!("pattern to find: {}", args.pattern);
-    println!("path where to search: {}", args.path.display());
+    // println!("pattern to find: {}", args.pattern);
+    // println!("path where to search: {}", args.path.display());
+    let content = std::fs::read_to_string(&args.path).expect("couldn't read file");
+    // .expect will be improved in future
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }    
 }
