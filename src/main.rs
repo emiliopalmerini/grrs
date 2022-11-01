98af2be3f701b62ceb46b20823943007d40d7321
emiliopalmerini <emilio.palmerini@gmail.com>
2022-11-01 13:18:31 +0100

    Added Parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
 }
