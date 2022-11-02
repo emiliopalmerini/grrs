967637233436ea776e710757549f203d8b3d6fa5
emilio <emilio.palmerini@codiceplastico.com>
2022-11-02 18:06:56 +0100

    Small improvment to error handling

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 #![allow(unused)]
 
 use clap::Parser;
+use anyhow::{Context, Result};
 
 #[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     // println!("pattern to find: {}", args.pattern);
     // println!("path where to search: {}", args.path.display());
-    let content = std::fs::read_to_string(&args.path).expect("couldn't read file");
+    let content = std::fs::read_to_string(&args.path).expect("couldn't read file")?;
+    };
+
     // .expect will be improved in future
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }    
+    Ok(())
 }
