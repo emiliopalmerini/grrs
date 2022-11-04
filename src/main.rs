791ded51cf506b88ac45d50c8c018091a3960206
emiliopalmerini <emilio.palmerini@gmail.com>
2022-11-04 08:58:38 +0100

    fixing merging

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,10 @@ struct Cli {
 }
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    // println!("pattern to find: {}", args.pattern);
-    // println!("path where to search: {}", args.path.display());
-    };
-
-
+    
     // read_to_string returns Result with an Error. Result is an enum 
     // .expect will be improved in future
-    let content = std::fs::read_to_string(&args.path).expect("couldn't read the file");
-    let content = match content {
-        Ok(s) => { content },
-        Err(e) => {panic!("Can't parse the file: {}", error)},
-
+    let content = std::fs::read_to_string(&args.path)?;
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
