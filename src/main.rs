8f8b6ad725bd3ce5e06ff80d0817a9cf2ae177b0
emiliopalmerini <emilio.palmerini@gmail.com>
2022-11-04 08:56:18 +0100

    resolved conflic with stashed change

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     // println!("pattern to find: {}", args.pattern);
     // println!("path where to search: {}", args.path.display());
-    let content = std::fs::read_to_string(&args.path).expect("couldn't read file")?;
     };
 
+
+    // read_to_string returns Result with an Error. Result is an enum 
     // .expect will be improved in future
+    let content = std::fs::read_to_string(&args.path).expect("couldn't read the file");
+    let content = match content {
+        Ok(s) => { content },
+        Err(e) => {panic!("Can't parse the file: {}", error)},
+
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
