32d9dd2f935810dd78aa24216fdcaf0c8e256e04
emiliopalmerini <emilio.palmerini@gmail.com>
2022-11-05 14:38:03 +0100

    improved error message

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 #![allow(unused)]
 
 use clap::Parser;
-// use anyhow::{Context, Result};
 
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Cli::parse();
-    
-    // read_to_string returns Result with an Error. Result is an enum 
-    // .expect will be improved in future
-    let content = std::fs::read_to_string(&args.path)?;
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }    
+
+#[derive(Debug)]
+struct CustomError(String);
+
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
     Ok(())
 }
